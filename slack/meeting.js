48df2f94cef79cf1a77dfc53e439b5ea850cb111
bdelpizzo <delpizzo.b@gmail.com>
2016-05-14 15:56:27 +0200

    Update initial function

--- slack/meeting.js
+++ slack/meeting.js
@@ -1,39 +1,42 @@
 var RtmClient = require('@slack/client').RtmClient;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var MemoryDataStore = require('@slack/client').MemoryDataStore;
 
-var token = "xoxb-41008753600-z2VsmKrZLAVHbrRn64EKYA0J";
 
-var rtm = new RtmClient(token, {logLevel: 'info'});
-rtm.start();
+var token = "xoxb-41008753600-dYXF74jVPvm8Ay6y09sYRZvb";
+
+var rtm = new RtmClient(token, {
+	logLevel: 'info',
+	dataStore: new MemoryDataStore(),
+	autoReconnect: true
+});
 
 
 rtm.on(RTM_EVENTS.MESSAGE, function (message) {
   
-  if(message.text.match(".*start meeting.*") {
-  	console.log("if is true");
-  }
-  else {
-  	console.log("if is false");
-  }
+var user = rtm.dataStore.getUserById(message.user)
+console.log(user.name);
 
+if(message.text.match(/.*start meeting*/)) {
+  	console.log('find start meeting on the channel: ', message.channel);
+  	  rtm.sendMessage('Let start the meeting', message.channel, function messageSent() {
+
+  });
+}
+  if (message.text.match(/.*check in*/)) {
+  	console.log('User checked ', message.user);
+   rtm.sendMessage('You are checked, follow this link ', message.channel, function messageSent() {
+	//TODO: ricevi il link
+  });
+}
+
+if (message.text.match(/.*stop*/)) {
+  	console.log('Good Bye!');
+ 	rtm.sendMessage('Good Bye!', message.channel, function messageSent() {
+	//TODO: ricevi il link
+  });}
+ 
 });
 
+rtm.start();
 
-
-console.log('User ID: ' , message.user);
-  console.log('Messaggio: ' , message.text);
-
-
-
-rtm.on(CLIENTS)
-
-
-rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
-  console.log(`Logged in as of  but not yet connected to a channel`);
-});
-
- if(message.text===".*start meeting.*")
-
-
-//regex CIAO aaa
-if(s.match(/.CIAO\s[a-zA-Z]/)) console.log("TRUE")
