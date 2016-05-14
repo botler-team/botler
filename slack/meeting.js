119089c8f39ee0d57cce6fadaf7c1168a27bd2ab
Faffola <raffymero@gmail.com>
2016-05-15 00:05:11 +0200

    sen message

--- slack/meeting.js
+++ slack/meeting.js
@@ -2,19 +2,20 @@ var RtmClient = require('@slack/client').RtmClient;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 var MemoryDataStore = require('@slack/client').MemoryDataStore;
 var partecipant;
+var chan = null;
 
 var token = "";
 
 var rtm = new RtmClient(token, {
 	logLevel: 'info',
-	dataStore: new MemoryDataStore(),
-	autoReconnect: true
+	dataStore: new MemoryDataStore()
 });
 
 rtm.on(RTM_EVENTS.MESSAGE, function (message) {
+  
   if(message.text.match(/.*start meeting*/)) {
     	console.log('find start meeting on the channel: ', message.channel);
-
+      chan = message.channel;
     	initialize();
 
     	  rtm.sendMessage('Let start the meeting', message.channel, function messageSent() {
@@ -35,6 +36,10 @@ rtm.on(RTM_EVENTS.MESSAGE, function (message) {
   	//TODO: ricevi il link
     });}
 
+    if (limitTalk(message) == true ){
+      chan = message.channel;
+      sendM("stai diventando prolisso!");
+    }
 });
 
 function initialize() {
@@ -43,21 +48,38 @@ function initialize() {
 
 
 function addMember(message){
-	var user = rtm.dataStore.getUserById(message.user);
-	var exists = false;
-	for(var i=0; i<partecipant.length && exists==false;i++)
-		if(partecipant[i].user==user) exists=true;
-	if(exists==false) {
-  	partecipant.push({user:user, channel:message.channel});
+
+ var user = rtm.dataStore.getUserById(message.user);
+ var exists = false;
+ for(var i=0; i<partecipant.length && exists==false;i++) 
+  if(partecipant[i].user==user) exists=true;
+ if(exists==false) {
+   partecipant.push({user:user, channel:message.channel});
   } else console.log("user aldready exists");
 }
 
-function sendMessage(text){
-  rtm.sendMessage(text, function messageSent() {
+
+function limitTalk(message){
+  var countWord = (message.text.toString().split(/.\s/)).length;
+  if (countWord > 30){
+    //sendM('Stai diventando prolisso');
+    console.log('Stai diventando prolisso..');
+    return true;
+  } else{
+    console.log('limit words false');
+    return false;
+
+  }
+}
+
+
+function sendM(text){
+  rtm.sendMessage(text, chan, function messageSent() {
     console.log('written');
   });
 }
 
-sendMessage('provadjhf dfljdfadfa');
+
 
 rtm.start();
+//sendM('provadjhf dfljdfadfa', chan);
