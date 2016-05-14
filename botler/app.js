11787908645d336e32449ef0a543346be4124115
MA <m.argenti@reply.it>
2016-05-14 17:29:03 +0200

    Added Slack feature to backend

--- botler/app.js
+++ botler/app.js
@@ -57,4 +57,72 @@ app.use(function(err, req, res, next) {
 });
 
 
+
+
+
+var RtmClient = require('@slack/client').RtmClient;
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var MemoryDataStore = require('@slack/client').MemoryDataStore;
+var partecipant;
+var channel;
+
+var token = "xoxb-41008753600-5pQZtKOFw3wM4GWdkw4ntLnf";
+
+var rtm = new RtmClient(token, {
+	logLevel: 'info',
+	dataStore: new MemoryDataStore(),
+	autoReconnect: true
+});
+
+rtm.on(RTM_EVENTS.MESSAGE, function (message) {
+  channel = !channel?message.channel:channel;
+  if(message.text.match(/.*start meeting*/)) {
+    	console.log('find start meeting on the channel: ', message.channel);
+    	initialize();
+    	rtm.sendMessage('Let start the meeting', message.channel, function messageSent() {
+    });
+  }
+    if (message.text.match(/.*check in*/)) {
+    	console.log('User checked ', message.user);
+    	addMember(message);
+      rtm.sendMessage('You are checked, follow this link ', message.channel, function messageSent() {
+  	//TODO: ricevi il link
+    });
+  }
+
+  if (message.text.match(/.*stop*/)) {
+    console.log('Good Bye!');
+   	rtm.sendMessage('Good Bye!', message.channel, function messageSent() {
+  	//TODO: ricevi il link
+    });}
+
+});
+
+function initialize() {
+	partecipant = [];
+}
+
+function sendMessage(text){
+  rtm.sendMessage(text, channel, function messageSent() {
+    console.log('written');
+  });
+}
+
+function addMember(message){
+	var user = rtm.dataStore.getUserById(message.user);
+	var exists = false;
+	for(var i=0; i<partecipant.length && exists==false;i++)
+		if(partecipant[i].user==user) exists=true;
+	if(exists==false) {
+  	partecipant.push({user:user, channel:message.channel});
+  } else console.log("user aldready exists");
+}
+
+rtm.start();
+
+
+
+
+
+
 module.exports = app;
--- slack/meeting.js
+++ slack/meeting.js
@@ -11,55 +11,53 @@ var rtm = new RtmClient(token, {
 	autoReconnect: true
 });
 
-
 rtm.on(RTM_EVENTS.MESSAGE, function (message) {
-  
+  if(message.text.match(/.*start meeting*/)) {
+    	console.log('find start meeting on the channel: ', message.channel);
 
+    	initialize();
 
-if(message.text.match(/.*start meeting*/)) {
-  	console.log('find start meeting on the channel: ', message.channel);
+    	  rtm.sendMessage('Let start the meeting', message.channel, function messageSent() {
 
-  	initialize();
-  	
-  	  rtm.sendMessage('Let start the meeting', message.channel, function messageSent() {
+    });
+  }
+    if (message.text.match(/.*check in*/)) {
+    	console.log('User checked ', message.user);
+    	addMember(message);
+      rtm.sendMessage('You are checked, follow this link ', message.channel, function messageSent() {
+  	//TODO: ricevi il link
+    });
+  }
 
-  });
-}
-  if (message.text.match(/.*check in*/)) {
-  	console.log('User checked ', message.user);
-  	addMember(message);
-    rtm.sendMessage('You are checked, follow this link ', message.channel, function messageSent() {
-	//TODO: ricevi il link
-  });
-}
+  if (message.text.match(/.*stop*/)) {
+    console.log('Good Bye!');
+   	rtm.sendMessage('Good Bye!', message.channel, function messageSent() {
+  	//TODO: ricevi il link
+    });}
 
-if (message.text.match(/.*stop*/)) {
-  	console.log('Good Bye!');
- 	rtm.sendMessage('Good Bye!', message.channel, function messageSent() {
-	//TODO: ricevi il link
-  });}
- 
 });
 
 function initialize() {
-	
 	partecipant = [];
-		
 }
 
 
 function addMember(message){
-
 	var user = rtm.dataStore.getUserById(message.user);
 	var exists = false;
-	for(var i=0; i<partecipant.length && exists==false;i++) 
+	for(var i=0; i<partecipant.length && exists==false;i++)
 		if(partecipant[i].user==user) exists=true;
 	if(exists==false) {
-
   	partecipant.push({user:user, channel:message.channel});
   } else console.log("user aldready exists");
 }
 
+function sendMessage(text){
+  rtm.sendMessage(text, function messageSent() {
+    console.log('written');
+  });
+}
+
+sendMessage('provadjhf dfljdfadfa');
+
 rtm.start();
-
-
