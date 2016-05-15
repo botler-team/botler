cd7cab3e4305bb8895dfe4dfb4a71dbf8665ab4e
Faffola <raffymero@gmail.com>
2016-05-15 04:19:05 +0200

    app update

---
+++ app_sentiment.js
@@ -0,0 +1,8 @@
+//npm install sentiment
+var sentiment = require('sentiment');
+
+var r1 = sentiment('Cats are stupid.');
+console.dir(r1);        // Score: -2, Comparative: -0.666
+
+var r2 = sentiment('Cats are totally amazing!');
+console.dir(r2);        // Score: 4, Comparative: 1
--- botler/app.js
+++ botler/app.js
@@ -203,73 +203,86 @@ var newMeetingEth = function() {
 var RtmClient = require('@slack/client').RtmClient;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 var MemoryDataStore = require('@slack/client').MemoryDataStore;
-var partecipant;
-var channel;
-
+partecipant=[];
+var chan = null;
 var token = "";
 
 var rtm = new RtmClient(token, {
-	logLevel: 'info',
-	dataStore: new MemoryDataStore(),
-	autoReconnect: true
+  logLevel: 'info',
+  dataStore: new MemoryDataStore()
 });
 
 rtm.on(RTM_EVENTS.MESSAGE, function (message) {
-  channel = !channel?message.channel:channel;
+ 
+  
   if(message.text.match(/.*start meeting*/)) {
-    	console.log('find start meeting on the channel: ', message.channel);
-    	initialize();
-    	rtm.sendMessage('Let start the meeting', message.channel, function messageSent() {
-    });
-    try{
-      newMeetingEth();
-    }catch(err){
-      console.log(err);
-    }
+      console.log('find start meeting on the channel: ', message.channel);
+      chan = message.channel;
+      initialize();
 
+        rtm.sendMessage('Let start the meeting', message.channel, function messageSent() {
+
+    });
   }
     if (message.text.match(/.*check in*/)) {
-    	console.log('User checked ', message.user);
-    	addMember(message);
+      console.log('User checked ', message.user);
+      addMember(message);
       rtm.sendMessage('You are checked, follow this link ', message.channel, function messageSent() {
-  	//TODO: ricevi il link
+    //TODO: ricevi il link
     });
   }
 
   if (message.text.match(/.*stop*/)) {
     console.log('Good Bye!');
-   	rtm.sendMessage('Good Bye!', message.channel, function messageSent() {
-  	//TODO: ricevi il link
+    rtm.sendMessage('Good Bye!', message.channel, function messageSent() {
+    //TODO: ricevi il link
     });}
 
+    if (limitTalk(message) == true ){
+      chan = message.channel;
+      sendM("stai diventando prolisso!");
+    }
 });
 
 function initialize() {
-	partecipant = [];
+  partecipant = [];
 }
 
-function sendMessage(text){
-  rtm.sendMessage(text, channel, function messageSent() {
+
+function addMember(message){
+
+ var user = rtm.dataStore.getUserById(message.user);
+ var exists = false;
+ for(var i=0; i<partecipant.length && exists==false;i++) 
+  if(partecipant[i].user==user) exists=true;
+ if(exists==false) {
+   partecipant.push({user:user, channel:message.channel});
+  } else console.log("user aldready exists");
+}
+
+
+function limitTalk(message){
+  var countWord = (message.text.toString().split(/.\s/)).length;
+  if (countWord > 30){
+    //sendM('Stai diventando prolisso');
+    console.log('Stai diventando prolisso..');
+    return true;
+  } else{
+    console.log('limit words false');
+    return false;
+
+  }
+}
+
+
+function sendM(text){
+  rtm.sendMessage(text, chan, function messageSent() {
     console.log('written');
   });
 }
 
-function addMember(message){
-	var user = rtm.dataStore.getUserById(message.user);
-	var exists = false;
-	for(var i=0; i<partecipant.length && exists==false;i++)
-		if(partecipant[i].user==user) exists=true;
-	if(exists==false) {
-  	partecipant.push({user:user, channel:message.channel});
-  } else console.log("user aldready exists");
 
-  try{
-    logParticipantEth(user);
-  }catch(err){
-    console.log(err);
-  }
 
-}
 
 rtm.start();
 
