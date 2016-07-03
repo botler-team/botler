7079977e9b8ccd99fbc35c4e45104b82019bd835
Giacomo Russo <gi.russo@reply.it>
2016-07-03 22:40:20 +0200

    Bugfix and full GCal functionality

--- botler/app.js
+++ botler/app.js
@@ -13,6 +13,11 @@ var github = require('octonode');
 var sentiment = require('sentiment');
 var nconf = require('nconf');
 
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+
 var app = express();
 
 // view engine setup
@@ -68,7 +73,22 @@ app.use(function(err, req, res, next) {
 
 nconf.argv().env();
 
+var SCOPES = ['https://www.googleapis.com/auth/calendar'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs.json';
+auth = null;
 
+// Load client secrets from a local file.
+fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+  if (err) {
+    console.log('Error loading client secret file: ' + err);
+    return;
+  }
+  // Authorize a client with the loaded credentials, then call the
+  // Google Calendar API.
+  authorize(JSON.parse(content));
+});
 
 /* Ethereum */
 var Web3 = require('web3');
@@ -224,6 +244,10 @@ var rtm = new RtmClient(token, {
 
 rtm.on(RTM_EVENTS.MESSAGE, function (message) {
 
+  if (message.text === undefined) {
+    return;
+  }
+
   if(message.text.match(/.*start meeting*/)) {
     	console.log('find start meeting on the channel: ', message.channel);
       chan = message.channel;
@@ -339,6 +363,11 @@ rtm.on(RTM_EVENTS.MESSAGE, function (message) {
     else if (message.text.match(/.*send invitation*/)) {
       // TODO Google Calendar code here
       var roomName = message.text.replace("send invitation", "");
+      var startDate = new Date();
+      startDate.setMinutes(0);
+      startDate.setSeconds(0);
+      startDate.setHours(startDate.getHours() + 1);
+      sendInvitationGCal(roomName, startDate);
     }
     else {
       logMeeting += message.text + " ";
@@ -394,7 +423,6 @@ function sendM(text){
 
 
 rtm.start();
-//sendM('provadjhf dfljdfadfa', chan);
 
 /* Tournment manager */
 currentUserIndex = 0;
@@ -518,22 +546,98 @@ function getGithubIssue(username){
 
 }
 
-function sendInvitationGCal(room) {
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials) {
+  var clientSecret = credentials.installed.client_secret;
+  var clientId = credentials.installed.client_id;
+  var redirectUrl = credentials.installed.redirect_uris[0];
+  var googleAuth2 = new googleAuth();
+  var oauth2Client = new googleAuth2.OAuth2(clientId, clientSecret, redirectUrl);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, function(err, token) {
+    if (err) {
+      getNewToken(oauth2Client);
+    } else {
+      oauth2Client.credentials = JSON.parse(token);
+      auth = oauth2Client;
+    }
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+function getNewToken(oauth2Client) {
+  var authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  });
+  console.log('Authorize this app by visiting this url: ', authUrl);
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('Enter the code from that page here: ', function(code) {
+    rl.close();
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err);
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+      auth = oauth2Client;
+    });
+  });
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+  try {
+    fs.mkdirSync(TOKEN_DIR);
+  } catch (err) {
+    if (err.code != 'EEXIST') {
+      throw err;
+    }
+  }
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+  console.log('Token stored to ' + TOKEN_PATH);
+}
+
+function sendInvitationGCal(room, dateTime) {
+  var endDate = new Date(dateTime);
+  endDate.setHours(endDate.getHours() + 1);
     var event = {
     'summary': 'Botler follow-up meeting',
     'location': 'Room: ' + room,
     'description': 'A chance to discuss more about the project status.',
     'start': {
-      'dateTime': '2015-05-28T09:00:00-07:00',
+      'dateTime': dateTime.toISOString(),
       'timeZone': 'Europe/Rome'
     },
     'end': {
-      'dateTime': '2015-05-28T17:00:00-07:00',
+      'dateTime': endDate.toISOString(),
       'timeZone': 'Europe/Rome'
     },
     'attendees': [
       {'email': 'gi.russo@reply.it'},
-      //{'email': 'm.argenti@reply.it'}
+      {'email': 'm.argenti@reply.it'}
     ],
     'reminders': {
       'useDefault': false,
@@ -543,18 +647,19 @@ function sendInvitationGCal(room) {
       ]
     }
   };
-
-  // calendar.events.insert({
-  //   auth: auth,
-  //   calendarId: 'primary',
-  //   resource: event,
-  // }, function(err, event) {
-  //   if (err) {
-  //     console.log('There was an error contacting the Calendar service: ' + err);
-  //     return;
-  //   }
-  //   console.log('Event created: %s', event.htmlLink);
-  // });
+  var calendar = google.calendar('v3');
+  calendar.events.insert({
+    auth: auth,
+    calendarId: 'primary',
+    resource: event,
+  }, function(err, event) {
+    if (err) {
+      console.log('There was an error contacting the Calendar service: ' + err);
+      return;
+    }
+    console.log('Event created: %s', event.htmlLink);
+    sendM('Event created: ' + event.htmlLink);
+  });
 }
 
 module.exports = app;
